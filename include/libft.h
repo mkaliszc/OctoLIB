3079bfc2f6ccae96673a86b49f0d20a955db6c7e
Milan Kaliszczak <mkaliszc@c1r5p4.42lehavre.fr>
2024-12-03 21:31:11 +0100

    ...

--- include/libft.h
+++ include/libft.h
@@ -58,7 +58,7 @@ char	**ft_split(char const *s, char c);
 
 void	ft_bzero(void *s, size_t n);
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
-void	ft_putchar_fd(char c, int fd);
+void	ft_putchar_fd(int c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
