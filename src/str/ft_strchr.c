513872f0f2a038d2ad450782bf61b3fae18cd702
Milan Kaliszczak <mkaliszc@c2r3p1.42lehavre.fr>
2025-01-07 18:37:27 +0100

    maj

--- src/str/ft_strchr.c
+++ src/str/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: mkaliszc <mkaliszc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 14:59:16 by mkaliszc          #+#    #+#             */
-/*   Updated: 2024/11/04 23:36:41 by mkaliszc         ###   ########.fr       */
+/*   Updated: 2025/01/07 18:37:17 by mkaliszc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strchr(const char *str, int c)
 	size_t		index;
 	char		*ptr;
 
+	if (!str)
+		return (NULL);
 	index = 0;
 	ptr = (char *)str;
 	while (index < (ft_strlen(str) + 1))
